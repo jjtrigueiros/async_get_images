3261be2f6099cbe031e36d7930e2ff4b28d2961b
J. Trigueiros <jjtrigueiros@gmail.com>
2023-04-07 16:45:39 +0100

    Fix PokeAPI integration to use pokemon-species
    The `/pokemon/` endpoint uses pokémon form-sensitive objects, such as
    giratina-altered and shaymin-land. The correct endpoint to refer to a
    particular species, regardless of form, is `/pokemon-species/`.

--- src/pokepapers/pokeapi.py
+++ src/pokepapers/pokeapi.py
@@ -8,17 +8,21 @@ class PokeAPI:
         self.api_url = settings.app.POKEAPI_URL
 
     def get_id(self, name: str) -> int:
-        return requests.get(f"{self.api_url}/pokemon/{name}").json().get("id")
+        return (
+            requests.get(f"{self.api_url}/pokemon-species/{name}").json().get("order")
+        )
 
     def get_name(self, id: int) -> str:
-        return requests.get(f"{self.api_url}/pokemon/{id}").json().get("name")
+        return requests.get(f"{self.api_url}/pokemon-species/{id}").json().get("name")
 
-    def list_pokemon(self) -> list[str]:
+    def list_pokemon(self, limit: int = 0) -> list[str]:
         """
         Returns an ordered (by national pokédex number) list of pokémon names.
         If this list is kept in memory, we can query a certain nat. dex ID
         with list[id-1].
         """
-        url = f"{self.api_url}/pokemon/?limit={settings.app.NUMBER_OF_POKEMON}"
+        if not limit:
+            limit = settings.app.NUMBER_OF_POKEMON
+        url = f"{self.api_url}/pokemon-species/?limit={limit}"
         api_data = requests.get(url).json()
         return [pkmn["name"] for pkmn in api_data["results"]]
